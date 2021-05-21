5b4ae50466a22d73ac183b3f03c7e0f81baa6eca
Kendrick Ang <kendrick.wh@outlook.com>
2021-05-21 11:32:25 +0800

    add javascript

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+})
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        // nav should only close if we click on it
+        document.body.classList.remove('nav-open');
+    })
+})
